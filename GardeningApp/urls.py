64b0076379b1c1654d3173154290c006a3a39c83
Caitlyn Pozerski <caitlyn.m.pozerski@gmail.com>
2021-04-28 00:36:31 -0700

    caitlyn's first commmit

--- GardeningApp/urls.py
+++ GardeningApp/urls.py
@@ -8,4 +8,15 @@ urlpatterns = [
     path('logout', views.logout),
     path('register', views.register),
     path('main', views.main),
+    #-----MARKETPLACE-----
+    path('market', views.market),
+    path('market/create', views.create_item),
+    path('market/<int:item_id>', views.one_item),
+    path('market/<int:item_id>/edit', views.edit_item),
+    path('market/<int:item_id>/update', views.update_item),
+    path('market/<int:item_id>/delete', views.delete_item),
+    path('market/cart', views.my_cart),
+    path('market/<int:item_id>/add', views.add_cart),
+    path('market/<int:item_id>/remove', views.remove_cart),
+    path('market/checkout', views.checkout),
 ]
